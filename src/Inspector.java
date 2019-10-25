e73f22e32c41c54471120d62c6259fcb0f19dfa4
Daniel <chiefgustavo000@gmail.com>
2019-10-24 23:29:38 -0600

    Completed the inspect constructor portion

--- src/Inspector.java
+++ src/Inspector.java
@@ -5,19 +5,19 @@ public class Inspector {
     public void inspect(Object obj, boolean recursive){
         Class c = obj.getClass();
         //System.out.println(c.getName());
-        inspectClass(c, obj, recursive, 0);
+        inspectClass(c, recursive, 0);
     }
 
-    private void inspectClass(Class c, Object obj, boolean recursive, int depth){
-        inspectSuperClass(c, obj, recursive, depth);
-        inspectInterface(c, obj, recursive, depth);
-        //inspectConstructor(c, obj, depth);
-        //inspectMethod(c, obj, depth);
-        //inspectField(c, obj, recursive, depth);
+    private void inspectClass(Class c, boolean recursive, int depth){
+        inspectSuperClass(c, recursive, depth);
+        inspectInterface(c, recursive, depth);
+        inspectConstructor(c, depth);
+        //inspectMethod(c, depth);
+        //inspectField(c, recursive, depth);
     }
 
-    private void inspectSuperClass(Class c, Object obj, boolean recursive, int depth){
-        System.out.println("inspecting super class of " + c);
+    private void inspectSuperClass(Class c, boolean recursive, int depth){
+        //System.out.println("inspecting super class of " + c);
         // handling formatting
         String tab = "";
         for(int i=0; i<depth; i++){
@@ -35,12 +35,12 @@ public class Inspector {
         }
         //print next superclass
         System.out.println(tab + "\tSUPERCLASS: " + superClass.getName());
-        inspectClass(superClass, obj, recursive, depth+2);
+        inspectClass(superClass, recursive, depth+2);
 
     }
 
-    private void inspectInterface(Class c, Object obj, boolean recursive, int depth){
-        System.out.println("inspecting interface for " + c);
+    private void inspectInterface(Class c, boolean recursive, int depth){
+        //System.out.println("inspecting interface for " + c);
         depth += 1;
         Class[] interfaces = c.getInterfaces();
 
@@ -51,27 +51,53 @@ public class Inspector {
         }
 
         if(interfaces.length==0){
-            System.out.println(tab + "NO INTERFACES FOR THIS CLASS");
+            System.out.println(tab + "*** NO SUPERINTERFACE ***");
         }else{
             // class does have interfaces
             for(int i = 0; i < interfaces.length; i++){
-                System.out.println("INTERFACE: " + interfaces[i]);
-                inspectInterface(interfaces[i], obj, recursive, depth);
-                //inspectConstructor(interfaces[i], obj,recursive, depth);
+                System.out.println(tab + "INTERFACE: " + interfaces[i]);
+                inspectInterface(interfaces[i], recursive, depth);
+                inspectMethod(interfaces[i], depth);
 
             }
         }
     }
 
-    private void inspectConstructor(Class c, Object obj, int depth){
+    private void inspectConstructor(Class c, int depth){
+        depth += 1;
+        // handling formatting
+        String tab = "";
+        for(int i=0; i<depth; i++){
+            tab += "\t";
+        }
+        String tab2 = tab + "\t";
+        // list of each constructors for a class
+        Constructor[] constructors = c.getConstructors();
+        for (int i = 0; i < constructors.length; i++){
+            System.out.println(tab + "CONSTRUCTOR:");
+            System.out.println(tab2 + "Name: " + constructors[i].getName());
 
+            //parameters
+            Class[] parameterTypes = constructors[i].getParameterTypes();
+            System.out.print(tab2 + "Parameter Types: ");
+            if (parameterTypes.length == 0){
+                System.out.println("None");
+            }else {
+                for (Class parameter : parameterTypes) {
+                    System.out.print(parameter.getName() + " ");
+                }
+                System.out.println("");
+            }
+
+            System.out.println(tab2 + "Modifier: " + constructors[i].getModifiers());
+        }
     }
 
-    private void inspectMethod(Class c, Object obj, int depth){
-
+    private void inspectMethod(Class c, int depth){
+        
     }
 
-    private void inspectField(Class c, Object obj, boolean recursive, int depth){
+    private void inspectField(Class c, boolean recursive, int depth){
 
     }
 
